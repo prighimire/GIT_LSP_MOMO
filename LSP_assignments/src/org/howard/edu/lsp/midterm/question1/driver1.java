f3057972dd77e55bdae0ca83818b642d3803b20d
prighimire <priyankaghimireee@gmail.com>
2024-03-14 20:42:09 -0400

    This is driver for assignment 1.

---
+++ LSP_assignments/src/org/howard/edu/lsp/midterm/question1/driver1.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class driver1 {
+    public static void main(String[] args) {
+        String originalText = "I love CSCI363";
+        String encryptedText = SecurityOps.encrypt(originalText);
+        System.out.println("Original Text: " + originalText);
+        System.out.println("Encrypted Text: " + encryptedText);
+    }
+}
